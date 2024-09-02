c8ef0c070e3b7fde51c6418d124562130e399f1b
Zara <122231647+zarail@users.noreply.github.com>
2024-09-02 12:53:12 +0200

    Update Home page with new message component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import HomePageMessage from "@/components/HomePageMessage";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "📍 Hey Local -- Home",
+  description: "Let me visit your city just like you do!",
+};
+
 export default function Home() {
-	return (
-		<main className="default-layout">
-			<h1>Willkommen zu Next</h1>
-		</main>
-	);
+  return (
+    <main className="default-layout">
+      <h1>Hey local! 👋</h1>
+      <HomePageMessage name="Zara" city="Barcelona" />
+    </main>
+  );
 }
