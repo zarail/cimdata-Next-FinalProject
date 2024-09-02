d685dabc38df8d74d0cc17fd2774f0feb9ba9091
Zara <122231647+zarail@users.noreply.github.com>
2024-09-02 12:52:56 +0200

    Add HomePageMessage component

---
+++ components/HomePageMessage.tsx
@@ -0,0 +1,29 @@
+type Props = {
+  name?: string;
+  city?: string;
+  visitTime?: string;
+};
+
+export default function HomePageMessage({
+  name = "Your friend",
+  city = "your city",
+  visitTime = "soon",
+}: Props) {
+  return (
+    <div className="homepage-message">
+      <p>
+        I’m going to visit {city} {visitTime} and would love your help to make
+        the most of my time there! Since you’re a local there, I’m sure you have
+        some amazing recommendations for me. 😊
+      </p>
+      <p>
+        I’d love to hear your top picks for each of these categories! Your
+        suggestions would really help me out:
+      </p>
+      <p>Thank a bunch 💐</p>
+      <p>{name}</p>
+    </div>
+  );
+}
+
+// maybe map the categories here to make the message more dynamic
