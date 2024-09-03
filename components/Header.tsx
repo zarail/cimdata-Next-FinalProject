75c744bbff6290d8cd39d183e2360751b119584f
Zara <122231647+zarail@users.noreply.github.com>
2024-09-03 11:05:36 +0200

    Add navigation link to Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+import { TiHome } from "react-icons/ti";
+
 export default function Header() {
-	return <header className="site-header">Header</header>;
+  return (
+    <header className="site-header">
+      <nav>
+        <Link legacyBehavior href="/">
+          <a href="/">
+            <TiHome />
+          </a>
+        </Link>
+      </nav>
+    </header>
+  );
 }
