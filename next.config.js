67312e9b4d2bac6af96f639249eac4b31ad842c3
Zara <122231647+zarail@users.noreply.github.com>
2024-09-03 16:18:00 +0200

    Add support for remote image patterns in next.config.js

--- next.config.js
+++ next.config.js
@@ -6,13 +6,17 @@ const nextConfig = {
     // aus der Standard Konfiguration (3840) zu verkleinern.
     // https://nextjs.org/docs/pages/api-reference/components/image#devicesizes
     deviceSizes: [640, 768, 1080, 1200, 1920, 2048, 2560],
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     // https://nextjs.org/docs/app/api-reference/components/image#remotepatterns
     remotePatterns: [
-      /*       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      }, */
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
   // https://nextjs.org/docs/app/building-your-application/routing/redirecting#redirects-in-nextconfigjs
