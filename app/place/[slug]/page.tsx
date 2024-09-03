6e8a1d0a4142012d3d282005edba7f772aa2a823
Zara <122231647+zarail@users.noreply.github.com>
2024-09-03 16:19:01 +0200

    Add new PlacePage component and styles

---
+++ app/place/[slug]/page.tsx
@@ -0,0 +1,72 @@
+import prisma from "@/prisma/db";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+import PlaceImage from "@/components/Place/PlaceImage";
+// import classes from "@/components/Place/PlaceImage.module.css";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function PlacePage({ params: { slug } }: Props) {
+  const placeData = await getPlaceData(slug);
+
+  if (!placeData) {
+    notFound();
+  }
+
+  const { name, description, street, number, zip, city, categories, images } =
+    placeData;
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{description}</p>
+      <p>
+        {street} {number}, {zip} {city}
+      </p>
+      <p>
+        Categories: {categories.map((category) => category.name).join(", ")}
+      </p>
+      {images.length > 0 && (
+        <div>
+          {images.map((image) => (
+            <PlaceImage key={image.id} name={name} {...image} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+async function getPlaceData(slug: string) {
+  const place = await prisma.place.findUnique({
+    where: {
+      slug,
+    },
+    include: {
+      categories: true,
+      images: true,
+    },
+  });
+
+  if (!place) {
+    notFound();
+  }
+
+  return place;
+}
+
+export async function generateMetadata({
+  params: { slug },
+}: Props): Promise<Metadata> {
+  const placeData = await getPlaceData(slug);
+
+  const name = placeData.name;
+
+  return {
+    title: `📍 ${name}`,
+  };
+}
