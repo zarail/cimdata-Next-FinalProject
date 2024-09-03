340ee8be0a9b7310ffe8f3031166f7bea574c107
Zara <122231647+zarail@users.noreply.github.com>
2024-09-03 16:19:12 +0200

    Refactor PlaceCard component to use Place type and update link URL

--- components/Place/PlaceCard.tsx
+++ components/Place/PlaceCard.tsx
@@ -1,16 +1,14 @@
 import Link from "next/link";
 import classes from "./PlaceCard.module.css";
+import type { Place } from "@prisma/client";
 
-type Props = {
-  place: string;
-};
+type Props = Place;
 
-export default function PlaceCard({ place }: Props) {
-  const placeURLClean = place.toLowerCase().replace(/\s+/g, "-");
+export default function PlaceCard({ name, slug }: Props) {
   return (
     <div className={classes.categoryCard}>
-      <Link className={classes.cardLink} href={`/place/${placeURLClean}`}>
-        <h3 className={classes.cardTitle}>{place}</h3>
+      <Link className={classes.cardLink} href={`/place/${slug}`}>
+        <h3 className={classes.cardTitle}>{name}</h3>
       </Link>
     </div>
   );
