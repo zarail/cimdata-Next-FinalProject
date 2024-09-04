ab7c77c2d6fd7f2fa5447782ef955bf86be977b8
Zara <122231647+zarail@users.noreply.github.com>
2024-09-04 09:28:52 +0200

    Add "Add New Place" link to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { TiHome } from "react-icons/ti";
+import { MdAddBox } from "react-icons/md";
 
 export default function Header() {
   return (
@@ -9,6 +10,9 @@ export default function Header() {
           <a href="/" className="header-link">
             <TiHome />
           </a>
+        </Link>
+        <Link href="/place/add-new" className="header-link">
+          <MdAddBox />
         </Link>
       </nav>
     </header>
