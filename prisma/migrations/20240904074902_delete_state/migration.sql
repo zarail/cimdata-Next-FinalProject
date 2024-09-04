14dece99bc8be94b173f19a6d016b39b657e2202
Zara <122231647+zarail@users.noreply.github.com>
2024-09-04 09:50:50 +0200

    Remove "state" column from "Place" table

---
+++ prisma/migrations/20240904074902_delete_state/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `state` on the `Place` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Place" DROP COLUMN "state";
