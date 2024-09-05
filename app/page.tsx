ca84aeeafcbdbce6e8b85bcd03eac80f0f8e56ca
Zara <122231647+zarail@users.noreply.github.com>
2024-09-05 16:25:54 +0200

    Update Home page layout to include sorted categories in a container

--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,19 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main className="default-layout">
-      <h1>Hey local! 👋</h1>
-      <HomePageMessage city="Cologne" />
-      <section>
-        <div className={classes.categoryGrid}>
-          {sortedCategories.map((category) => (
-            <li key={category}>
-              <CategoryCard key={category} category={category} />
-            </li>
-          ))}
-        </div>
-      </section>
+      <div className="container">
+        <h3>Hey local! 👋</h3>
+        <HomePageMessage city="Cologne" />
+        <section>
+          <div className={classes.categoryGrid}>
+            {sortedCategories.map((category) => (
+              <li key={category}>
+                <CategoryCard key={category} category={category} />
+              </li>
+            ))}
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
