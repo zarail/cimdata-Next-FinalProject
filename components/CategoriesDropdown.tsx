bb61c87336d47ba821e8f9242713b8047ab59f4f
Zara <122231647+zarail@users.noreply.github.com>
2024-09-05 14:35:35 +0200

    Add CategoriesDropdown component

---
+++ components/CategoriesDropdown.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+type CategoriesDropdownProps = {
+  categories: string[];
+  onItemClick: () => void;
+};
+
+export default function CategoriesDropdown({
+  categories,
+  onItemClick,
+}: CategoriesDropdownProps) {
+  return (
+    <div className="categories-dropdown">
+      <ul>
+        {categories.map((category, index) => (
+          <li key={index}>
+            <Link href={`/category/${category}`} onClick={onItemClick}>
+              {category}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
