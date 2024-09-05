99661d764453bc7ed4d1aafa76059689cf797ad8
Zara <122231647+zarail@users.noreply.github.com>
2024-09-05 14:35:09 +0200

    Update header component with improved logo and categories dropdown

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,48 @@
-import Link from "next/link";
-import { TiHome } from "react-icons/ti";
-import { MdAddBox } from "react-icons/md";
+"use client";
 
-export default function Header() {
+import Link from "next/link";
+import { MdHome } from "react-icons/md";
+import { MdAddBox } from "react-icons/md";
+import { TbCategoryFilled } from "react-icons/tb";
+import { useToggle } from "@/lib/hooks/useToggle";
+import CategoriesDropdown from "./CategoriesDropdown";
+
+type HeaderProps = {
+  categories: string[];
+};
+
+export default function Header({ categories }: HeaderProps) {
+  const [isDropdownOpen, toggleDropdown, , , setDropdownClose] =
+    useToggle(false);
+
   return (
     <header className="site-header">
-      <nav>
-        <Link legacyBehavior href="/">
-          <a href="/" className="header-link">
-            <TiHome />
+      <div className="logo-container">
+        <Link href="/" className="header-link">
+          <MdHome size={30} />
+          <strong> Hey Local</strong>
+        </Link>
+        <div className="categories-container">
+          <a
+            className="header-link"
+            onClick={toggleDropdown}
+            style={{ cursor: "pointer" }}
+          >
+            <TbCategoryFilled size={30} />
+            <strong>Categories</strong>
           </a>
-        </Link>
+          {isDropdownOpen && (
+            <CategoriesDropdown
+              categories={categories}
+              onItemClick={setDropdownClose}
+            />
+          )}
+        </div>
         <Link href="/place/add-new" className="header-link">
-          <MdAddBox />
+          <MdAddBox size={30} />
+          <strong> Add a new place</strong>
         </Link>
-      </nav>
+      </div>
     </header>
   );
 }
