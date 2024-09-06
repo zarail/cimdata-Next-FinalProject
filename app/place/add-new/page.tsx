2ad08d31d5f9d4b3d8735e8fef7f77092374422f
Zara <122231647+zarail@users.noreply.github.com>
2024-09-06 10:25:17 +0200

    Add user authentication to AddNewPage

--- app/place/add-new/page.tsx
+++ app/place/add-new/page.tsx
@@ -1,12 +1,14 @@
 import AddNewForm from "@/components/Place/AddNewForm";
 import prisma from "@/prisma/db";
 import type { Metadata } from "next";
+import { auth } from "@/auth";
 
 export const metadata: Metadata = {
   title: "📍 Add New Place",
 };
 
 export default async function AddNewPage({}) {
+  const session = await auth();
   const categories = await prisma.category.findMany({
     select: {
       id: true,
@@ -28,7 +30,11 @@ export default async function AddNewPage({}) {
   return (
     <div className="container">
       <h3>Add a new place</h3>
-      <AddNewForm categories={categories} images={images} />
+      {session ? (
+        <AddNewForm categories={categories} images={images} />
+      ) : (
+        <h6>Sorry! You need to be logged in to add a new place.</h6>
+      )}
     </div>
   );
 }
