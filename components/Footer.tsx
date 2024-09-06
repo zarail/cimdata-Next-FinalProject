eb4d1bde09d52de5c07d0a4d39de5e521c46ffb5
Zara <122231647+zarail@users.noreply.github.com>
2024-09-06 10:24:44 +0200

    Update Footer component to include user authentication and session information

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,19 @@
+import { auth } from "@/auth";
+import { SignIn } from "./Auth/SignIn";
+import { SignOut } from "./Auth/SignOut";
+import User from "./Auth/User";
+
 export default async function Footer() {
+  const session = await auth();
+  console.log(session);
+  console.log(session?.user);
+  console.log(session?.user?.name);
+
   return (
     <footer className="site-footer">
-      📍 Hey Local - Developed by Zahra Ilkhan - 2024
+      <strong>📍 Hey Local - Developed by Zahra Ilkhan - 2024</strong>
+      {session && <User {...session.user} />}
+      {session ? <SignOut /> : <SignIn />}
     </footer>
   );
 }
